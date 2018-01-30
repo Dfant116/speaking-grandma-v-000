f063088aab714cc07fca71703b1f55e58cf55a43
Dalton Fant <daltonfant16@gmail.com>
2018-01-30 21:59:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else
+  elsif 
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
