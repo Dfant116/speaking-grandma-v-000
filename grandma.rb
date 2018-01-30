53801affc636aaba72140b598234a638f035dd6a
Dalton Fant <daltonfant16@gmail.com>
2018-01-30 22:10:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
-  end
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
