0e763f4ad438adf45e2000e0b2e69b2a16331f17
Dalton Fant <daltonfant16@gmail.com>
2018-01-30 21:18:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (phrase)
-  if "#{phrase}" 
+  if "#{phrase}"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif "#{phrase}".upcase 
+  elsif "#{phrase}".upcase
     puts "NO, NOT SINCE 1938!"
   else "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
-  end 
+  end
 end
